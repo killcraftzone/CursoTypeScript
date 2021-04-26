e47775068699d93598741fd12a4776398304f519
killcraftzone <ohxanaina959@gmail.com>
2021-04-26 03:23:48 -0300

    Curso TypeScript

---
+++ index.ts
@@ -0,0 +1,20 @@
+function greet(name) {
+
+    return `Olá ${name}`;
+
+}
+
+console.log(greet('Guilherme Pires'));
+
+
+/*------------------------------------------------------------*/
+
+/*
+
+// Sair do ambiente global de programação
+
+const nome1 = 'Guilherme'
+
+export default nome1;
+
+*/
