b2d655636286440abbc4f5fecd4e3036756b04a9
killcraftzone <ohxanaina959@gmail.com>
2021-04-29 02:58:14 -0300

    teste

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+/*function greet(name) {
+
+    return `Olá ${name}`;
+
+}
+
+console.log(greet('Guilherme Pires'));
+
+*/
+
+/*------------------------------------------------------------*/
+
+/*
+
+// Sair do ambiente global de programação
+
+const nome1 = 'Guilherme'
+
+export default nome1;
+
+*/
+
+/*------------------------------------------------------------*/
+
+//const nome = 'Guilherme';
+//const sobrenome = 'Pires';
+
+
+/*------------------------------------------------------------*/
+
+import funcao from './mod';
+
+funcao();
